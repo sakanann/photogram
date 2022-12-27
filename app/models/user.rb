be1d0fd2d09ecffd1891e2eff5b5d953615c4a6a
takuya sakamoto <sthtcwm@yahoo.co.jp>
2022-12-27 17:42:14 +0900

    ユーザーの値がバリデーション判定される前にemail属性を強制的に小文字に変換

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  class User < ApplicationRecord
     validates :name,  presence: true, length: { maximum: 30 }
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  end
+    before_validation { email.downcase! } 
 end
