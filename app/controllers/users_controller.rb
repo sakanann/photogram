67a89649a7714a19961f529e2b548fdcd6059579
takuya sakamoto <sthtcwm@yahoo.co.jp>
2022-12-27 18:13:18 +0900

    add:showアクション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,16 @@ class UsersController < ApplicationController
   end
 
   def create
-      @user = User.new(user_params)
-      if @user.save
-        
-      else
-        render :new
-      end
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
