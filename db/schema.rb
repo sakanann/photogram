7f32957b5f90fd893c82ca355a34bc7cc979f860
takuya sakamoto <sthtcwm@yahoo.co.jp>
2022-12-28 22:07:19 +0900

    imageカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_28_032930) do
+ActiveRecord::Schema.define(version: 2022_12_28_130203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2022_12_28_032930) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.text "image"
     t.index ["user_id"], name: "index_pictures_on_user_id"
   end
 
