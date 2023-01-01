215504b840633f4a2ce8f7bd5d51621382a70084
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-01-01 23:43:55 +0900

    足掻き：プロフィール編集が更新できない模索中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,21 +6,37 @@ class UsersController < ApplicationController
   end
 
   def create
-      @user = User.new(user_params)
-      if @user.save
-        redirect_to user_path(@user.id)
-      else
-        render :new
-      end
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
   end
 
   def show
-      @user = User.find(params[:id])
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    unless @user.id = current_user.id
+      redirect_to new_picture_path
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path, notice: "プロフィールを編集しました！"
+		else
+      render :edit
+		end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
   end
 end
